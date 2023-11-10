8a657591e10c3f6447109965db97e3c3730fffc2
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-11-10 16:51:43 -0600

    object cajero

--- CajeroAutomatico/CajeroAutomaticoMain.java
+++ CajeroAutomatico/CajeroAutomaticoMain.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class CajeroAutomaticoMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        CajeroAutomatico cajero = new CajeroAutomatico();
     }
 }
