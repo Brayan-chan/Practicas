13a13d3ae0c2e2ba9d40768a6d5e93013f1fc182
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-11-10 16:54:48 -0600

    commit

--- CajeroAutomatico/CajeroAutomatico.java
+++ CajeroAutomatico/CajeroAutomatico.java
@@ -1,5 +1,32 @@
 package CajeroAutomatico;
 
+import java.util.List;
+
 public class CajeroAutomatico {
-    
+    private static final String BILLETES_FILE = "billetes.dat";
+    private static final String LOGS_FILE = "logs.txt";
+
+    private Usuario usuario;
+    private List<Billete> billetes;
+
+    public void cargarBilletesIniciales() {
+    }
+
+    public void guardarBilletes() {
+    }
+
+    public void consultarSaldo() {
+    }
+
+    public void retirarEfectivo(double cantidad) {
+    }
+
+    public void registrarLog(String accion, String usuario, double saldo, boolean seRealizo) {
+    }
+
+    public void mostrarLogs() {
+    }
+
+    public void mostrarBilletesDisponibles() {
+    }
 }
