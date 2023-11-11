cc1b217bdbece0ee7f94a16a18f6db79810bbe3c
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-11-10 21:57:37 -0600

    SaldoAleatorio

--- CajeroAutomatico/Usuario.java
+++ CajeroAutomatico/Usuario.java
@@ -1,5 +1,34 @@
 package CajeroAutomatico;
+import java.io.Serializable;
 
-public class Usuario {
-    
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String nombre;
+    private int nip;
+    private double saldo;
+
+    public Usuario(String nombre, int nip) {
+        this.nombre = nombre;
+        this.nip = nip;
+        // Math.random para generar un saldo aleatorio entre $1,000 y $50,000
+        this.saldo = Math.random() * (50000 - 1000) + 1000; 
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNip() {
+        return nip;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    // Método para retirar saldo
+    public void retirarSaldo(double cantidad) {
+        saldo -= cantidad;
+    }
 }
