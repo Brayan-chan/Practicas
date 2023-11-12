bd0a8b8cd992c3dde59fda344c73c76e72cc7e1c
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-11-11 21:31:59 -0600

    menuAdministrador

--- CajeroAutomatico/Administrador.java
+++ CajeroAutomatico/Administrador.java
@@ -6,6 +6,32 @@ import java.io.IOException;
 
 public class Administrador implements Serializable {
     private static final String LOGS_FILE = "logs.txt";
+
+    public void menuAdministrador(CajeroAutomatico cajero) {
+        int opcion;
+        do {
+            System.out.println("\nMenú Administrador:");
+            System.out.println("1. Mostrar listado de acciones");
+            System.out.println("2. Mostrar cantidad de billetes disponibles");
+            System.out.println("3. Salir");
+            System.out.print("Ingrese la opción: ");
+            opcion = CajeroAutomatico.scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    mostrarListadoAcciones();
+                    break;
+                case 2:
+                    mostrarCantidadBilletesDisponibles(cajero);
+                    break;
+                case 3:
+                    System.out.println("Sesión de administrador finalizada.");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+            }
+        } while (opcion != 3);
+    }
     
     public void mostrarListadoAcciones() {
         System.out.println("Listado de acciones:");
